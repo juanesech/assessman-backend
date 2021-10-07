d709dc82223908db752877440e7431935a5703b2
Juanes Echavarria <juanesech@gmail.com>
2021-10-07 18:12:59 -0500

    add questions api

--- ms-assessments/functions/src/index.ts
+++ ms-assessments/functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as admin from "firebase-admin";
 import {newAssessment, listAssessments, getAssessment} from "./assessments";
+import {newQuestion, listQuestions} from "./questions";
 
 admin.initializeApp();
 
 exports.addAssessment = newAssessment();
 exports.listAssessments = listAssessments();
 exports.getAssessment = getAssessment();
+exports.addQuestion = newQuestion();
+exports.listQuestions = listQuestions();
---
+++ ms-assessments/functions/src/questions.ts
@@ -0,0 +1,40 @@
+import * as functions from "firebase-functions";
+import {firestore} from "firebase-admin";
+
+
+interface Question {
+    content: string,
+    type: string
+    
+};
+
+/**
+ * Saves question into a firebase document.
+ * @return {functions.HttpsFunction} a firebase https function
+ */
+export function newQuestion(): functions.HttpsFunction{
+    return functions.https.onRequest(async (req, res) => {
+        const body: Question = req.body;
+        const writeQuestion= await firestore()
+            .collection("questions")
+            .add(body);
+        res.json({result: `Question with ID: ${writeQuestion.id} added.`});
+    });
+}
+
+/**
+ * Lists saved questions.
+ * @return {functions.HttpsFunction} a firebase https function
+ */
+ export function listQuestions(): functions.HttpsFunction {
+    return functions.https.onRequest(async (req, res) => {
+      const questionsList = await firestore()
+          .collection("questions")
+          .get()
+          .then((querySnap) => {
+            return querySnap.docs
+                .map( (doc) => ({data: doc.data(), id: doc.id}));
+          });
+      res.json(questionsList);
+    });
+}
